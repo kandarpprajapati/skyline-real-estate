e41881fcbb02a457d7c12995bb5c6983506669fa
kandarpprajapati <https://github.com/kandarpprajapati>
2023-04-24 21:35:24 +0530

    homepage card changes

--- pages/index.js
+++ pages/index.js
@@ -1038,7 +1038,7 @@ export default function Home() {
               >
                 <img
                   alt="Home"
-                  src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  src="https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
                   class="h-56 w-full rounded-md object-cover"
                 />
 
@@ -1137,7 +1137,7 @@ export default function Home() {
               >
                 <img
                   alt="Home"
-                  src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  src="https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
                   class="h-56 w-full rounded-md object-cover"
                 />
 
